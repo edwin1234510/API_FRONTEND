55a7ee99883375289e40dce27174bbaa5875d14b
edwin villabona <edwinvillabona0701@gmial.com>
2025-05-23 08:02:14 -0500

    clase

--- src/middlewares/usuarios/campos.js
+++ src/middlewares/usuarios/campos.js
@@ -1,9 +1,9 @@
 export const campos = [
-    {name: "documento", required: true, minLength: 8, maxLength: 11},
-    {name: "nombre", required: true, minLength: 3, maxLength: 20},
-    {name: "apellido", required: true, minLength: 3, maxLength: 20},
-    {name: "telefono", required: true, minLength: 10, maxLength: 11},
-    {name: "contrasena", required: true, minLength: 5, maxLength: 20},
-    {name: "id_genero", required: true,type: "number", minLength: 1, maxLength: 20},
-    {name: "id_ciudad", required: true,type: "number", minLength: 1, maxLength: 20}
+  { name: "documento", required: true, minLength: 8, maxLength: 11 },
+  { name: "nombre", required: true, minLength: 3, maxLength: 50 },
+  { name: "apellido", required: true, minLength: 3, maxLength: 50 },
+  { name: "telefono", required: true, minLength: 10, maxLength: 10 },
+  { name: "contrasena", required: true, minLength: 8 },
+  { name: "id_genero", required: true, type: "number" },
+  { name: "id_ciudad", required: true, type: "number" }
 ]
