9446001a7559b2b7a6e21cfe52ae3c13aee85ead
edwin villabona <edwinvillabona0701@gmial.com>
2025-05-23 07:51:41 -0500

    trabajo en clase

--- src/service/servicioUsuarios.js
+++ src/service/servicioUsuarios.js
@@ -56,10 +56,10 @@ class UsuariosServicio {
       };
     }
   }
-  static async createUsuarioS(documento,nombre, apellido, telefono, contrasena, id_genero, id_ciudad) {
+  static async createUsuarioS(documento, nombre, apellido, telefono, contrasena, id_genero, id_ciudad) {
     try {
       const UsuariosInstancia = new Usuarios();
-      const usuarios = await UsuariosInstancia.create(documento,nombre, apellido, telefono, contrasena, id_genero, id_ciudad);
+      const usuarios = await UsuariosInstancia.create(documento, nombre, apellido, telefono, contrasena, id_genero, id_ciudad);
       if (usuarios === null) {
         return {
           error: true,
@@ -85,14 +85,14 @@ class UsuariosServicio {
     try {
       const UsuariosInstancia = new Usuarios();
       const usuariosExis = await UsuariosInstancia.getById(id);
-      if(usuariosExis.length === 0){
-        return{
+      if (usuariosExis.length === 0) {
+        return {
           error: true,
           code: 404,
           message: "Usuario no encontrada",
         }
       }
-      const usuario = await UsuariosInstancia.update(id,campos);
+      const usuario = await UsuariosInstancia.update(id, campos);
       if (usuario === null) {
         return {
           error: true,
@@ -114,28 +114,20 @@ class UsuariosServicio {
       }
     }
   }
-  static async deleteUsuarioS(id){
+  static async deleteUsuarioS(id) {
     try {
       const UsuariosInstancia = new Usuarios();
       const usuariosExis = await UsuariosInstancia.getById(id);
-      if(usuariosExis.length === 0){
-        return{
+      if (usuariosExis.length === 0) {
+        return {
           error: true,
           code: 404,
           message: "Usuario no encontrada",
         }
       }
-      const usuarios = await UsuariosInstancia.LenguajeUsuarios(id);
-      if(usuarios.length > 0){
-        return{
-          error: true,
-          code: 400,
-          message: "No se puede eliminar el usuario porque tiene lenguajes asociados. Elimine primero las relaciones en Usuarios_Lenguajes.",
-        }
-      }
       const rta = await UsuariosInstancia.eliminar(id);
-      if(rta.error){
-        return{
+      if (rta.error) {
+        return {
           error: true,
           code: 400,
           message: rta.mensaje,
